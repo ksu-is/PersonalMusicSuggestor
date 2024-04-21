99cba8590e24512e76d7341bdd60608c366bb6ab
brake223 <bmcgaugh@students.kennesaw.edu>
2024-04-21 03:30:07 -0400

    Update PersonalMusicSuggestor.py

--- PersonalMusicSuggestor.py
+++ PersonalMusicSuggestor.py
@@ -1,1 +1,12 @@
 #testing codespace
+def get_music_recommendation(score):
+#Determines music recommendation based on the score, NEXT ill input quiz questions and results ideas
+    if score < 3:
+        return 
+    elif score < 6:
+        return 
+    elif score < 9:
+        return 
+    else:
+        return 
+
